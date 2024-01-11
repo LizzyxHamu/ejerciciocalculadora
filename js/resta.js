e8882f6baa0b9b2c74b956267284986d71f8d0b3
Marco Hernandez <marc.hernandeza@duocuc.cl>
2024-01-10 21:03:28 -0300

    commit js

--- js/resta.js
+++ js/resta.js
@@ -1,10 +1,13 @@
-function sumar(){
+function resta(){
+
     var v1=parseInt(document.getElementById("valor1").value);
     var v2=parseInt(document.getElementById("valor2").value);
-    var suma=0;
 
-    suma=v1+v2;
+    var resta = 0;
 
-    document.getElementById("resultado").value=suma;
+    resta = v1 - v2;
+
+    document.getElementById("resultado").value=resta;
 
 }
+
